2cf5a3cce38364bd7e8807faf32d0b5eac5561bb
Bernardo <bernardotmota@gmail.com>
2022-03-08 20:49:55 -0300

    first commit

---
+++ DIO/Enum/Genero.cs
@@ -0,0 +1,20 @@
+namespace DIO.Series
+{
+    public enum Genero
+    {
+        Acao = 1,
+        Aventura = 2,
+        COmedia = 3,
+        Documetario = 4,
+        Drama = 5,
+        Espionagem = 6,
+        Faroeste = 7,
+        Fantasia = 8,
+        FiccaoCientifica = 9,
+        Musical = 10,
+        Romance = 11,
+        Suspense = 12,
+        Terror = 13
+    }
+    
+}
